ee6caf3fd40c9b8253367d0aea2d919f13af54f7
ali <ali@goldriverpoker.com>
2019-09-18 14:14:18 +0430

    25-address get

--- Controllers/AddressController.js
+++ Controllers/AddressController.js
@@ -21,37 +21,19 @@ exports.AddNewAddress =  async function (req , res) {
 
 };
 
-exports.GetAddresses = function (req , res) {
+exports.GetAddresses = async function (req , res) {
 
-    if(req.body.ActivationCode === 0) {
-        res.status(500).send({error : true , code : "wrong_activation_code" , message : "Wrong Activation Code"});
-        return;
-    }
+    var User = await AuthorizationToken.Decrypt(req.get("Authorization"));
 
-    AddressSchema.findOne({PhoneNumber : req.body.PhoneNumber } , function (err , data) {
+
+
+
+    AddressSchema.find({UserID : User._id } , function (err , data) {
         if(err) {
             res.status(500).send({error : true , code : "system_error" , message : "some error happens"});
             return;
         }
-        if(data == null) {
-            res.status(500).send({error : true , code : "user_not_found" , message : "User Not Found"});
-        }
-        else if(data.Activated) {
-            res.status(500).send({error : true , code : "user_already_activated" , message : "User Already Activated"});
-        }
-        else if(data.ActivationCode === req.body.ActivationCode) {
-            data.ActivationCode = 0 ;
-            data.Activated = true ;
-
-            var token = jwt.sign({data : data}, '123456');
-
-            data.save().then(() => res.send({error : false , code : "success_activation" ,
-                    message : "Activation Successfull" , Token : token } ) );
-        }
-        else {
-            res.status(500).send({error : true , code : "wrong_activation_code" , message : "Wrong Activation Code"});
-
-        }
+        res.send(data);
     });
 };
 
